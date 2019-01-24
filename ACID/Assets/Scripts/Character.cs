1645a278cca5a10e439bfe61a4c742bc41fdc025
Last Abyss <contact@lasts-mbp.intra.ecu.edu>
2019-01-24 15:24:29 -0500

    added a basic player move and mouse look script. Downloaded a modular spaceship package.

---
+++ ACID/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float Speed;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = transform.TransformDirection(move);
+        move.y = 0;
+        _controller.Move(move * Time.deltaTime * Speed);
+    }
+}
