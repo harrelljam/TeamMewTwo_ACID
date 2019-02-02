f56105e189503dfc381374d6c068207fdd5c6751
jedagen <hunter.duzac@gmail.com>
2019-02-02 15:27:04 -0500

    New robot chaaracter model, lighting, rooms, etc.

--- ACID/Assets/Scripts/Door/AutoDoor.cs
+++ ACID/Assets/Scripts/Door/AutoDoor.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class AutoDoor : MonoBehaviour
 {
     private Animator _anim;
+    private AudioSource _aud;
+    public bool HasAudioSource;
 
     private void Start()
     {
         _anim = GetComponent<Animator>();
+        if(HasAudioSource)
+            _aud = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.CompareTag("Player"))
         {
+            if(HasAudioSource)
+                _aud.Play();
             _anim.SetBool("character_nearby", true);
         }
     }
