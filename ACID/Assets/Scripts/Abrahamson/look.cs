a271e8cce439a65dad41cef958b65181de2e9300
Frig <ericvanhallen@gmail.com>
2019-03-21 17:16:26 -0400

    Created a Radius Damage script which can be applied to anything with a Trigger Collider

---
+++ ACID/Assets/Scripts/Abrahamson/look.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class look : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.player.transform.position;
+        pos.y += 10;
+        plane.transform.LookAt(pos, Vector3.down);
+    }
+}
---
+++ ACID/Assets/Scripts/RadiusDmg/RadiusDamage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiusDamage : MonoBehaviour
+{
+	public GameObject player;
+	private bool inside;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.inside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(this.inside){
+        	Debug.Log("ahhh I'm burning");
+        }*/
+    }
+    
+    private void onTriggerEnter(Collider obj){
+    	if(obj.gameObject.CompareTag("Player")){
+    		this.inside = true;
+    		Debug.Log("I'm burning");
+    	}
+    }
+    
+    private void onTriggerExit(Collider obj){
+    	if(obj.gameObject.CompareTag("Player")){
+    		this.inside = false;
+    		Debug.Log("Left");
+    	}
+    }
+}
