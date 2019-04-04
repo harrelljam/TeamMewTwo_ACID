ce6f7ce41e456a5642898f76b3f121848d47e235
jedagen <hunter.duzac@gmail.com>
2019-04-04 17:41:20 -0400

    added back james' flashlight

---
+++ ACID/Assets/Scripts/Flashlight/Flashlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+	public GameObject flashlight;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.F)){
+        	flashlight.active = !flashlight.active;
+        }
+    }
+}
