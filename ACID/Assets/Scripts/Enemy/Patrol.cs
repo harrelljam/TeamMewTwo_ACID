2bf1f6412cae94476408220a4bf8630954b59dc6
jedagen <hunter.duzac@gmail.com>
2019-04-04 01:51:05 -0400

    robot finds, chases, and ceases to chase. no lose state yet

--- ACID/Assets/Scripts/Enemy/Patrol.cs
+++ ACID/Assets/Scripts/Enemy/Patrol.cs
@@ -4,43 +4,101 @@ using UnityEngine.AI;
 using System.Collections;
 
 
-public class Patrol : MonoBehaviour {
+public class Patrol : MonoBehaviour
+{
 
-    public Transform[] points;
-    private int destPoint = 0;
-    private NavMeshAgent agent;
+    public float WalkSpeed;
+    public float ChaseSpeed;
+    public float PauseTime;
+    public Animator Anim;
+    public Transform[] Points;
+    public AudioClip[] RobotScreams;
+    private int _destPoint = 0;
+    private NavMeshAgent _agent;
+    private bool _waiting = false;
+    private bool _chasing = false;
+    private Transform _target;
+    private AudioSource _source;
 
 
-    void Start () {
-        agent = GetComponent<NavMeshAgent>();
+    private void Start ()
+    {
+        _source = this.GetComponent<AudioSource>();
+        _agent = GetComponent<NavMeshAgent>();
 
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
         // approaches a destination point).
-        agent.autoBraking = false;
+        _agent.autoBraking = false;
 
         GotoNextPoint();
     }
 
 
-    void GotoNextPoint() {
+    private void GotoNextPoint() {
         // Returns if no points have been set up
-        if (points.Length == 0)
+        if (Points.Length == 0)
             return;
-
+        _agent.isStopped = false;
         // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
+        _agent.destination = Points[_destPoint].position;
 
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        _destPoint = (_destPoint + 1) % Points.Length;
+        Anim.SetBool("moving", true);
     }
 
+    public void ChasePlayer(Transform player)
+    {
+        _target = player;
+        _chasing = true;
+        _agent.destination = player.position;
+        _agent.speed = ChaseSpeed;
+        Anim.SetBool("chasing", true);
+        Anim.SetBool("moving", true);
+        _source.clip = RobotScreams[Random.Range(0, RobotScreams.Length)];
+        _source.Play();
+    }
+    public void LosePlayer()
+    {
+        _chasing = false;
+        _target = null;
+        _agent.speed = WalkSpeed;
+        Anim.SetBool("chasing", false);
+        Anim.SetBool("moving", false);
+        _agent.isStopped = true;
+        StartCoroutine(WaitHere(PauseTime));
+    }
 
-    void Update () {
+    private void Update () {
         // Choose the next destination point when the agent gets
         // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f && !_waiting && !_chasing)
+        {
+            _agent.isStopped = true;
+            StartCoroutine(WaitHere(PauseTime));
+            _waiting = true;   
+        }
+
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f && _chasing)
+        {
+            print("you lose");
+        }
+        else if (!_agent.pathPending && _agent.remainingDistance < 2f && _chasing)
+        {
+            _agent.destination = _target.position;
+        }
+    }
+
+    private IEnumerator WaitHere(float seconds)
+    {
+        Anim.SetBool("moving", false);
+        yield return new WaitForSeconds(seconds);
+        if (!_chasing)
+        {
+            _waiting = false;
+            GotoNextPoint();   
+        }
     }
 }
---
+++ ACID/Assets/Scripts/Enemy/Sight.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    public Patrol Patrol;
+    public float SightDist;
+    public LayerMask Layer;
+
+    private bool _inSight;
+    private Transform _target;
+
+    private void Update()
+    {
+        if (_inSight)
+        {
+            var heading = _target.position - transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance;
+            if (!Physics.Raycast(transform.position, direction, out var hit, SightDist, Layer))
+            {
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        _target = null;
+        _inSight = false;
+        Patrol.LosePlayer();
+    }
+
+    private void NewTarget(Transform target)
+    {
+        _target = target.transform;
+        _inSight = true;
+        Patrol.ChasePlayer(_target);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            print(other.name + " entered");
+            var heading = other.transform.position - transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance;
+            if (Physics.Raycast(transform.position, direction, out var hit, SightDist, Layer))
+            {
+                NewTarget(other.transform);
+            }
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            if (_inSight)
+            {
+                Reset();
+            }
+        }
+    }
+}
