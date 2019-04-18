b1afb2c9f30a47854380d67474e69558e6157c80
Hawkinsj15 <hawkinsj15@students.ecu.edu>
2019-04-18 17:27:56 -0400

    o

---
+++ ACID/Assets/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
